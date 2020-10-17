8b275ff0e6cbb5e6893e016eb943faf7e897c59e
PDU Demo Center <cjshono@gmail.com>
2020-10-16 22:17:37 -0700

    merged prep_model_data script from corey-dev

---
+++ projection_model/prep_model_data.py
@@ -0,0 +1,52 @@
+"""
+This code defines functions for reading raw CSV data pulled from the webscraper
+and cleans it into data frame formats that can be fed into a predictinve ML mode.
+The outputs from this model are:
+- train_df.csv
+- val_df.csv
+- test_df.csv
+Each dataframe will have all of the variables (columns) and samplevplayer-weeks
+(rows) required to train/fit/predict/evaluate a given dataset.
+"""
+
+import pandas as pd
+import pickle
+import os
+import re
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+import gc
+
+class globs():
+    dir_players = "./data/player_weeks/"
+    dir_opp = "./data/opp_weeks/"
+    dir_salaries = "./data/fanduel_salaries/"
+    dir_weather = "./data/nfl_weather/"
+    dir_snapcounts = "./data/snapcounts/"
+    dir_benchmark = "./data/fanduel_projections/" 
+
+class WeeklyStatsYear():
+    """
+    This class is holds a year's worth of weekly stats. The stats stored in This
+    class store all of the variable inputs (X's) as well as the targets (y's),
+    and in some cases, the bench mark (y_bench's) that are fed into the ML
+    prediction model.
+    """
+    def __init__(self, year):
+        self.year = year
+
+    def read_player_data(filepath):
+        self.df_player = pd.read_csv(filepath)
+
+    def read_opp_data(filepath):
+        self.df_opp = pd.read_csv(filepath)
+
+    def read_salaries_data(filepath):
+        self.df_salaries = pd.read_csv(filepath)
+
+    def read_snapcounts_data(filepath):
+        self.df_snapcounts = pd.read_csv(filepath)
+
+    def read_benchmark_data(filepath):
+        self.df_benchmark = pd.read_csv(filepath)
